e1e3fa1364e51ef140c592c2e6cda2042f0b704e
jdhrnndz <johndeniellehernandez@gmail.com>
2016-04-26 20:44:30 +0800

    Updated config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  */
 
 export default {
-  title: 'React Static Boilerplate',
-  description: 'Static website generator for React.js web applications.',
+  title: 'LSD',
+  description: 'Labor Statistics Dashboard',
   googleAnalyticsId: 'UA-XXXXX-X',
 };
